c88f13753728bca395686fe4666338104becf74e
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-09-24 11:59:34 +0530

    If else in java

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[] args) {
+        boolean issunup = false;
+        if (issunup == true)
+            System.out.println("Day");
+
+        else
+            System.out.println("night");
+
+    }
+}
