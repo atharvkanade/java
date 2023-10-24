08ebde0d0a5ec83ee237ae7b3b5575171fd4f607
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-10-24 23:58:42 +0530

    Switch in java

---
+++ switch.java
@@ -0,0 +1,27 @@
+
+
+Public class Switch {
+  public static void main(String[] args) {
+
+    String language = "Java";
+
+    switch(language) {
+
+      case "Java":
+        System.out.println(language + " is famous for enterprise applications.");
+        break;
+
+      case "JavaScript":
+        System.out.println(language + " is famous for frontend and backend.");
+        break;
+
+      case "Python":
+        System.out.println(language + " is famous for ML and AI.");
+        break;
+
+      default:
+        System.out.println(language + " not found on record.");
+        break;
+    }
+  }
+}
