18b83aa8930f9ec7f72af6f4a5334cb37aa27375
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-11-03 23:57:51 +0530

    String operation.

--- string.java
+++ string.java
@@ -2,9 +2,9 @@ public class string {
     public static void main(String[] args) {
         String message = new String("hello world :");
 
-        System.out.println(message.endsWith(":"));
-        System.out.println(message.indexOf("l", 0));
-        System.out.println(message.replace("l", "b"));
+        System.out.println(message.endsWith(":")); //To find string end with 
+        System.out.println(message.indexOf("l", 0)); //To find index of character
+        System.out.println(message.replace("l", "b"));//To replace char
         System.out.println(message.toUpperCase());
 
     }
