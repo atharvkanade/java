5fb37bb2a98bb727b870f06e78ae7fa10bbf9d96
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-11-08 21:45:30 +0530

    Input reading in java

---
+++ input.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Enter your age :");
+        byte age = Scanner.nextByte();
+        System.out.println("Your age is " + age);
+    }
+}
