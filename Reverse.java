1730c89e8b96af17c0b31f1f7aea10ca0b065ed0
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-11-22 23:37:26 +0530

    Reverse number

--- Reverse.java
+++ Reverse.java
@@ -1,13 +1,18 @@
-Public class Main {
+public class Reverse {
   public static void main(String[] args) {
 
     int num = 1234, reversed = 0;
     
     System.out.println("Original Number: " + num);
+
+   
     while(num != 0) {
     
+     
       int digit = num % 10;
       reversed = reversed * 10 + digit;
+
+  
       num /= 10;
     }
 
