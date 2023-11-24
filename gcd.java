761458ed6032707fc5163bfd05dc6c877ded2fbe
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-11-24 23:59:25 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,17 +1,20 @@
-public class gcd {
+public class Main {
+
   public static void main(String[] args) {
 
-   
-    int n1 = 81, n2 = 153;
-    int gcd = 1;
+    // positive number
+    int number = 60;
 
-    for (int i = 1; i <= n1 && i <= n2; ++i) {
+    System.out.print("Factors of " + number + " are: ");
 
-      
-      if (n1 % i == 0 && n2 % i == 0)
-        gcd = i;
+    // loop runs from 1 to 60
+    for (int i = 1; i <= number; ++i) {
+
+      // if number is divided by i
+      // i is the factor
+      if (number % i == 0) {
+        System.out.print(i + " ");
+      }
     }
-
-    System.out.println("GCD of " + n1 +" and " + n2 + " is " + gcd);
   }
 }
