49196f40d17733454cf77d96104f91cca0cf7f81
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-12-02 22:12:02 +0530

    Prime number

--- Prime.java
+++ Prime.java
@@ -13,7 +13,7 @@ public class Prime {
 
     if (!flag)
       System.out.println(num + " is a prime number.");
-    else
+    else 
       System.out.println(num + " is not a prime number.");
   }
 }
