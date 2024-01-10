523d6e765a5d7802b93928c9f8bb78f8fbc42bc1
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-01-10 11:56:50 +0530

    Getter and setter in java

---
+++ getset.java
@@ -0,0 +1,44 @@
+// main class
+public class practice {
+    public static void main(String[] args) {
+        var employee = new employee();
+        employee.setBasesalary(50000);
+        employee.setHourlyrate(50);
+        int wage = employee.calculatewage(10);
+        System.out.println(wage);
+
+    }
+}
+
+// employeeclass
+public class employee {
+    private int basesalary;
+    private int hourlyrate;
+
+    public int calculatewage(int extrahours) {
+        return basesalary + (extrahours * hourlyrate);
+    }
+
+    private int getBasesalary() {
+        return basesalary;
+    }
+
+    public void setBasesalary(int basesalary) {
+        if (basesalary <= 0) {
+            throw new IllegalArgumentException("Salary can't be 0 or -ve");
+        }
+        this.basesalary = basesalary;
+    }
+
+    private int getHourlyrate() {
+        return hourlyrate;
+    }
+
+    public void setHourlyrate(int hourlyrate) {
+        if (hourlyrate <= 0) {
+            throw new IllegalArgumentException("Hourly rate can't be 0 or -ve");
+
+        }
+        this.hourlyrate = hourlyrate;
+    }
+}
