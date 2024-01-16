ab539de4a17511974b66c3fbfa0c3e7306f8c17f
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-01-16 23:49:58 +0530

    Create linkedlist in java

---
+++ linkedlist.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class linkedkist {
+  public static void main(String[] args){
+
+    // create linkedlist
+    LinkedList<String> primeno = new LinkedList<>();
+    primeno.add("2");
+    primeno.add("3");
+    primeno.add("5");
+    System.out.println("LinkedList: " + primeno);
+  }
+}
