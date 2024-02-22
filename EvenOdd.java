02de97d95a682618a61be5b1c819076649001acd
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-02-22 23:31:03 +0530

    Even Odd in java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
-
 public class EvenOdd {
-
     public static void main(String[] args) {
-
         Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
+        System.out.print("Enter the  number: ");
         int num = reader.nextInt();
-
         if(num % 2 == 0)
-            System.out.println(num + " is even");
+            System.out.println(num + " is an even number.");
         else
-            System.out.println(num + " is odd");
+            System.out.println(num + " is an odd number.");
     }
 }
