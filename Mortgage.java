438e164d8aeabd38891a61c1ce571f8840575cda
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-02 23:46:21 +0530

    Mortgage calculator

--- Mortgage.java
+++ Mortgage.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class Mortgage {
     public static void main(String[] args) {
         Scanner Scanner = new Scanner(System.in);
         System.out.println("Enter the principle amount :");
         int principle = Scanner.nextInt();
-        System.out.println("Enter Annual Interest : ");
+        System.out.println("Enter the  Annual Interest : ");
 
         float interest = Scanner.nextFloat();
         System.out.println("Enter the time period (in years) :");
@@ -14,14 +13,13 @@ public class Mortgage {
         int p = principle;
        /*12 months
        100 for percentage*/
-        
         float i = (interest / 100 / 12);
         int y = (period * 12);
         float x = (float) Math.pow((1 + i), y);
 
         float mortgage = p * ((i * x) / (x - 1));
 
-        System.out.println("Your mortgage is :" + mortgage);
+        System.out.println("Your mortgage amount is :" + mortgage);
 
     }
 
