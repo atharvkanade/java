2978d625fcb427cb44546be5f1f61331cfe23e52
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-05 23:20:11 +0530

    Find GCD of numbers

--- Gcd.java
+++ Gcd.java
@@ -1,11 +1,7 @@
-public class GCD {
-
+public class Gcd {
   public static void main(String[] args) {
-
-    int number = 60;
-
-    System.out.print("Factors of " + number + " are: ");
-
+    int number = 196;
+    System.out.print("Factors of the " + number + " are: ");
     for (int i = 1; i <= number; ++i) {
       if (number % i == 0) {
         System.out.print(i + " ");
