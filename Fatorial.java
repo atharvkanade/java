30d340ff149799635dc1829c01b1d41efe0d1de1
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-08 23:57:50 +0530

    Factorial in java

--- Fatorial.java
+++ Fatorial.java
@@ -1,13 +1,11 @@
 public class Factorial {
-
     public static void main(String[] args) {
-
-        int num = 10;
+        int num = 15;
         long fact = 1;
         for(int i = 1; i <= num; ++i)
         {  
             fact *= i;
         }
-        System.out.printf("Factorial of %d = %d", num, fact);
+        System.out.printf("Factorial of number %d = %d", num, fact);
     }
 }
