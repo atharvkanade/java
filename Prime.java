7de415f8d8d3470a83aba489e71f810cbb9ca394
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-12 23:54:28 +0530

    Prime number in java

--- Prime.java
+++ Prime.java
@@ -1,6 +1,6 @@
 public class Prime {
   public static void main(String[] args) {
-    int num = 29;
+    int num = 13;
     boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
       if (num % i == 0) {
@@ -9,8 +9,8 @@ public class Prime {
       }
     }
     if (!flag)
-      System.out.println(num + " is a prime number.");
+      System.out.println("The given"num + " is a prime number.");
     else 
-      System.out.println(num + " is not a prime number.");
+      System.out.println("The given"num + " is not a prime number.");
   }
 }
