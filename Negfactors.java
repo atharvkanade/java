51dad77e7bfec9d57ea55ccbcd7e6d6957db3464
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-19 23:40:31 +0530

    Factors of negative number

--- Negfactors.java
+++ Negfactors.java
@@ -1,13 +1,8 @@
 class Negfactors {
-
   public static void main(String[] args) {
-    int number = -60;
-    System.out.print("Factors of " + number + " are: ");
-
-  
+    int number = -180;
+    System.out.print("Factors of the " + number + " are as: ");
     for(int i = number; i <= Math.abs(number); ++i) {
-
-   
       if(i == 0) {
         continue;
       }
