f1d464b0b0087245fd624be90d98d9b690435471
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-22 23:20:49 +0530

    Multiplication Table

--- Multiplication.java
+++ Multiplication.java
@@ -2,9 +2,10 @@ public class Multiplication {
 
     public static void main(String[] args) {
 
-        int num = 5;
+        int num = 19;
         for(int i = 1; i <= 10; ++i)
         {
+            System.out.printf("Multiplication Table");
             System.out.printf("%d * %d = %d \n", num, i, num * i);
         }
     }
