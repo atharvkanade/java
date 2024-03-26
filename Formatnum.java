2940cae80a61f5e9ebf23746fe2c9f89a0415272
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-26 23:48:35 +0530

    Formating of number

--- Formatnum.java
+++ Formatnum.java
@@ -1,13 +1,13 @@
 import java.text.NumberFormat;
 
-public class practice {
+public class Formatnum {
     public static void main(String[] args) {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(1259545.453);
+        String result = currency.format(1245595.453);
         System.out.println(result);
 
         NumberFormat percentage = NumberFormat.getPercentInstance();
-        String result2 = percentage.format(.86);
+        String result2 = percentage.format(.68);
         System.out.println(result2);
     }
 }
