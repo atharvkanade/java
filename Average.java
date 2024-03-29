0dbf75329ba577f480d7020ac5e1b32a7ac93989
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-29 23:34:40 +0530

    Average of an array

--- Average.java
+++ Average.java
@@ -1,13 +1,11 @@
 public class Average {
 
     public static void main(String[] args) {
-        double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0, 45.6 };
+        double[] numArray = { 456.3, 678.5, -485.6, 260.34, 383.0, 152.6 };
         double sum = 0.0;
-
         for (double num: numArray) {
            sum += num;
         }
-
         double average = sum / numArray.length;
         System.out.format("The average is: %.2f", average);
     }
