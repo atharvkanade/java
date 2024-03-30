83ef5439f54dc300a1f54ca49a9eb7587cd652f5
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-30 23:27:44 +0530

    Palindrome string

--- Palindrome.java
+++ Palindrome.java
@@ -1,19 +1,16 @@
 Public class Palindrome {
   public static void main(String[] args) {
-
-    String str = "Comic", reverseStr = "";
-    
+    String str = "Atharv", reverseStr = "";
     int strLength = str.length();
-
     for (int i = (strLength - 1); i >=0; --i) {
       reverseStr = reverseStr + str.charAt(i);
     }
 
     if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
-      System.out.println(str + " is a Palindrome String.");
+      System.out.println("Given"str + " is a Palindrome String.");
     }
     else {
-      System.out.println(str + " is not a Palindrome String.");
+      System.out.println("Given"str + " is not a Palindrome String.");
     }
   }
 }
