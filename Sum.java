9633655b3bfe846faf89ab964b45c054a9920eab
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-03-31 23:06:18 +0530

    Sum upto n number

--- Sum.java
+++ Sum.java
@@ -1,15 +1,11 @@
 public class Sum {
 
     public static void main(String[] args) {
-
-        int num = 10, sum = 0;
-
+        int num = 15, sum = 0;
         for(int i = 1; i <= num; ++i)
-        {
-            
+         {
             sum += i;
-        }
-
-        System.out.println("Sum = " + sum);
+         }
+        System.out.println("Sum upto the " num "= " + sum);
     }
 }
