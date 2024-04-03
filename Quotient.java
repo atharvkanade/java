b544ab6e3caf4a462b34848625c8f8e3a15aa1d3
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-04-03 23:48:24 +0530

    To find quotient and reminder

--- Quotient.java
+++ Quotient.java
@@ -1,13 +1,9 @@
 public class Quotient {
-
   public static void main(String[] args) {
-
-    int dividend = 25, divisor = 4;
-
+    int dividend = 100, divisor = 25;
     int quotient = dividend / divisor;
     int remainder = dividend % divisor;
-
-    System.out.println("Quotient = " + quotient);
-    System.out.println("Remainder = " + remainder);
+    System.out.println("Quotient  is = " + quotient);
+    System.out.println("Remainder is = " + remainder);
   }
 }
