c0d9d5c2bbc89561c3d5aaf5fd02d30321805daa
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-04-09 23:36:05 +0530

    Find factorial of number

--- Fatorial.java
+++ Fatorial.java
@@ -1,11 +1,11 @@
 public class Factorial {
     public static void main(String[] args) {
-        int num = 15;
+        int num = 20;
         long fact = 1;
         for(int i = 1; i <= num; ++i)
         {  
             fact *= i;
         }
-        System.out.printf("Factorial of number %d = %d", num, fact);
+        System.out.printf("Factorial of the given number %d  is = %d", num, fact);
     }
 }
