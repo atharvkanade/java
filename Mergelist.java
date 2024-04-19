e08f4c654e3d64f060ea98d6a5bc4f73b89d0f49
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2024-04-19 23:57:31 +0530

    Merge the list in java

--- Mergelist.java
+++ Mergelist.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Mergelist {
+public class Mergelist {
   public static void main(String[] args) {
     List<Integer> prime = new ArrayList<>();
-    prime.add(2);
-    prime.add(3);
-    prime.add(5);
+    prime.add(7);
+    prime.add(11);
+    prime.add(23);
     System.out.println("First List: " + prime);
     List<Integer> even = new ArrayList<>();
-    even.add(4);
-    even.add(6);
+    even.add(22);
+    even.add(82);
     System.out.println("Second List: " + even);
     List<Integer> numbers = new ArrayList<>();
     numbers.addAll(prime);
     numbers.addAll(even);
 
-    System.out.println("Merged List: " + numbers);
+    System.out.println("Merged List is : " + numbers);
 
   }
 }
